a6b91c0d2c6ff3c83e70c96995662597bc5ee124
marzia <you@example.com>
2025-05-30 12:09:13 +0200

    Initialize project with HTML and JavaScript files

---
+++ snack3/main.js
@@ -0,0 +1,12 @@
+// il software deve chiedere per 10 volte all'utente di inserire un numero. il programma stampa la somma di tutti i numeri inseriti.
+
+// 1 ciclo for per chiedere... povero utente
+// - ciclo for
+// -
+// 2 somma
+
+// let ask = prompt("inserire un numero");
+
+for (let ask = prompt("inserire un numero"); ask <= 10; ask++) {
+  console.log(ask);
+}
