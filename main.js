7789d057f80751cc8a5761de4c1be2c67cc03541
marzia <you@example.com>
2025-05-30 10:53:48 +0200

    Add functionality to compare two user-input numbers and log the greater one

---
+++ main.js
@@ -0,0 +1,12 @@
+// l'utente inserisce due numeri in successione con due prompt
+// il software stampa il maggiore
+
+const numberOne = Number(prompt("numero 1 "));
+const numberTwo = Number(prompt("numero 2"));
+// console.log(numberOne, numberTwo);
+
+if (numberOne > numberTwo) {
+  console.log(numberOne);
+} else {
+  console.log(numberTwo);
+}
