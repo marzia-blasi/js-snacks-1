0c612e64d3c8e4d2282511eac7b6f77cf4a62dd6
marzia <you@example.com>
2025-05-30 12:34:57 +0200

    Add loop to prompt user for number input six times

--- snack4/main.js
+++ snack4/main.js
@@ -1,3 +1,9 @@
 // crea un array vuoto chiedi per 6 volte di inserire un numero, se è dispari inseriscilo nell'array
 
 let list = [];
+
+let number = 6;
+
+for (i = number; i >= 1; i--) {
+  let ask = Number(prompt("inserire numero"));
+}
